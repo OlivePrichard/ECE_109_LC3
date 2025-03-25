3d9af7bc6eb9ba4259fdffaff2caa4d210049a44
Olive Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2025-03-25 00:43:37 -0400

    no longer crashes, my fix made it different but idk if it's better

--- initial_formatting.py
+++ initial_formatting.py
@@ -5,7 +5,7 @@ def read_image():
         for i in range(1, 4):
             file.readline()
 
-        for i in range(4, 170):
+        for i in range(4, 177):
             line = file.readline()
             key = line[1:3]
             r = int(line[7:9], 16)
@@ -16,7 +16,7 @@ def read_image():
             else:
                 print(f"Error on line {i}")
         
-        for i in range(170, 250):
+        for i in range(177, 257):
             image.append([])
             line = file.readline()
             length = line[1:].find('"')
@@ -45,8 +45,8 @@ def trim_image(image):
 def compress(image):
     reduction = 256 // 32
     linear = []
-    for col in range(len(image[0])):
-        for row in range(len(image)):
+    for row in range(len(image)):
+        for col in range(len(image[row])):
             linear.append(image[row][col] //reduction)
     compressed = []
     i = 0
@@ -63,6 +63,10 @@ def compress(image):
         else:
             compressed.append(linear[i] + 32 * linear[i + 1] + 32**2 * linear[i + 2] + 32**3)
             i += 3
+            if linear[i + 2] == 0:
+                i -= 1
+                if linear[i + 1] == 0:
+                    i -= 1
     return compressed
 
 def alt_compress(image):
@@ -100,16 +104,44 @@ def lossy_compress(image):
 def write_to_file(values):
     with open('initials.asm', 'w') as file:
         file.write('.ORIG x0\n')
-        for value in values:
+        for i, value in enumerate(values):
             file.write(' ' * 8 + f'.FILL x{value:04X}\n')
+            if i % 128 == 127:
+                file.write('\n')
         file.write('.END')
 
+def read(values):
+    c = 0
+    pixels = []
+    for value in values:
+        if value >= 2**15:
+            v = value - 2**15
+            for _ in range(3):
+                pixels.append(v % 32)
+                v //= 32
+                if v == 0:
+                    break
+        else:
+            p = value % 32
+            if p == 31:
+                c += 1
+            for _ in range(value // 32 + 1):
+                pixels.append(p)
+    
+    print(len(pixels))
+    # write_to_file(pixels)
+    for i, pixel in enumerate(pixels):
+        print('#' if pixel > 15 else ' ', end='')
+        if i % 128 == 127:
+            print()
+
 def main():
-    image = trim_image(read_image())
+    image = read_image()
     print(f'Image is {len(image[0])}px by {len(image)}px')
     compressed = compress(image)
     print(f'Compressed image is {len(compressed)} words long')
     write_to_file(compressed)
+    read(compressed)
 
 if __name__ == '__main__':
     main()
